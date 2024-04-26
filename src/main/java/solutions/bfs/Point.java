adce395b306944d4c8d8cde6f80b4e5427e9cd1d
shachopin <shachopin@gmail.com>
2024-04-26 15:45:10 +0000

    added point

---
+++ src/main/java/solutions/bfs/Point.java
@@ -0,0 +1,10 @@
+package solutions.bfs;
+
+class Point {
+    int x;
+    int y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/solutions/bfs/ZombieMatrixSolution.java
+++ src/main/java/solutions/bfs/ZombieMatrixSolution.java
@@ -95,12 +95,3 @@ public class ZombieMatrixSolution {
         }
     }
 }
-
-class Point {
-    int x;
-    int y;
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
